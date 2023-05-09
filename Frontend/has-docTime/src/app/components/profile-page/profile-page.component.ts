04be8ce4129eb145036b83a10180a8187e9b2c5f
Mihir Joshi <mjoshi@argusoft.com>
2023-05-09 10:57:33 +0530

    [updated profile page with cdr detect changes]

--- Frontend/has-docTime/src/app/components/profile-page/profile-page.component.ts
+++ Frontend/has-docTime/src/app/components/profile-page/profile-page.component.ts
@@ -13,16 +13,16 @@ import { DoctorService } from 'src/app/services/doctor.service';
 })
 export class ProfilePageComponent implements OnInit, OnDestroy {
 
-  constructor(private datePipe : DatePipe, private userService: UserService, private cdr: ChangeDetectorRef,
-    private doctorService: DoctorService){
+  constructor(private datePipe: DatePipe, private userService: UserService, private cdr: ChangeDetectorRef,
+    private doctorService: DoctorService) {
 
   }
 
   user?: User;
   doctor?: Doctor;
   disable: boolean = true;
-  firstName : string = "";
-  lastName : string = "";
+  firstName: string = "";
+  lastName: string = "";
   id: number = 0;
   tokenRole: string = "";
   dateFromAPI = new Date('2022-05-12T00:00:00');
@@ -32,23 +32,23 @@ export class ProfilePageComponent implements OnInit, OnDestroy {
 
     const token = sessionStorage.getItem('token');
     console.log(token);
-    if(token){
+    if (token) {
 
       // console.log();
-      
+
       let store = token?.split('.');
       this.tokenRole = atob(store[1]).split(',')[2].split(':')[1];
       console.log(atob(store[1]));
-      this.id = parseInt(atob(store[1]).split(',')[1].split(':')[1].substring(1, this.tokenRole.length -1)); 
-      
-      this.tokenRole = this.tokenRole.substring(1,this.tokenRole.length - 1);
+      this.id = parseInt(atob(store[1]).split(',')[1].split(':')[1].substring(1, this.tokenRole.length - 1));
+
+      this.tokenRole = this.tokenRole.substring(1, this.tokenRole.length - 1);
       console.log(this.tokenRole);
-      if(this.tokenRole === "DOCTOR"){
+      if (this.tokenRole === "DOCTOR") {
         // console.log("doctor called");
-        
+
         this.doctorService.getDoctor(this.id).subscribe(data => {
-          
-          
+
+
           this.doctor = data;
           this.user = data.user;
           console.log(this.doctor);
@@ -69,13 +69,13 @@ export class ProfilePageComponent implements OnInit, OnDestroy {
             height: this.doctor.user.height,
             weight: this.doctor.user.weight,
             dob: this.formattedDate,
-            qualification : this.doctor.qualification,
-            casesSolved : this.doctor.casesSolved
+            qualification: this.doctor.qualification,
+            casesSolved: this.doctor.casesSolved
           });
         });
       }
-      
-      if(this.tokenRole === "PATIENT"){
+
+      if (this.tokenRole === "PATIENT") {
         this.userService.getUser().subscribe(data => {
           this.user = data;
           this.id = data.id as number;
@@ -85,12 +85,12 @@ export class ProfilePageComponent implements OnInit, OnDestroy {
           this.lastName = nameArray[1];
           this.dateFromAPI = new Date(data.dob);
           console.log("SDFGFGFDGFD", this.dateFromAPI);
-          
+
           this.formattedDate = this.datePipe.transform(this.dateFromAPI, 'yyyy-MM-dd');
           console.log("formated ", this.formattedDate);
           console.log("user", this.user);
           console.log(this.user?.bloodGroup);
-    
+
           this.editForm.patchValue({
             firstName: this.firstName,
             lastName: this.lastName,
@@ -102,84 +102,99 @@ export class ProfilePageComponent implements OnInit, OnDestroy {
             weight: this.user.weight,
             dob: this.formattedDate
           });
-          
+
         });
       }
 
     }
-    
-   
+
+
 
   }
 
-  editForm : FormGroup = new FormGroup({
-    firstName : new FormControl({value: "", disabled : this.disable}, [Validators.required]),
-    lastName : new FormControl({value: "", disabled : this.disable} , [Validators.required]),
-    dob : new FormControl({value: "", disabled : this.disable}, [Validators.required]),
-    gender : new FormControl({value: "",disabled : this.disable}, [Validators.required]),
-    bloodGroup : new FormControl({value: "",disabled : this.disable}, [Validators.required]),
-    contact : new FormControl({value: "",disabled : this.disable}, [Validators.required]),
-    height : new FormControl({value: "",disabled : this.disable}),
-    weight : new FormControl({value: "", disabled : this.disable}),
-    email : new FormControl({value: "",disabled : this.disable}, [Validators.required]),
-    qualification : new FormControl({value: "",disabled : this.disable}),
-    casesSolved : new FormControl({value: "",disabled : true}),
-    patientChronicIllness : new FormArray([])
+  editForm: FormGroup = new FormGroup({
+    firstName: new FormControl({ value: "", disabled: this.disable }, [Validators.required]),
+    lastName: new FormControl({ value: "", disabled: this.disable }, [Validators.required]),
+    dob: new FormControl({ value: "", disabled: this.disable }, [Validators.required]),
+    gender: new FormControl({ value: "", disabled: this.disable }, [Validators.required]),
+    bloodGroup: new FormControl({ value: "", disabled: this.disable }, [Validators.required]),
+    contact: new FormControl({ value: "", disabled: this.disable }, [Validators.required]),
+    height: new FormControl({ value: "", disabled: this.disable }),
+    weight: new FormControl({ value: "", disabled: this.disable }),
+    email: new FormControl({ value: "", disabled: this.disable }, [Validators.required]),
+    qualification: new FormControl({ value: "", disabled: this.disable }),
+    casesSolved: new FormControl({ value: "", disabled: true }),
+    patientChronicIllness: new FormArray([])
   })
 
-  
+
 
   ngOnDestroy(): void {
-      
+
   }
 
-  onSubmit(){
-    
+  onSubmit() {
+
     console.log("sanket");
     const date = new Date();
 
     const user = this.editForm.value;
     console.log(user);
-    
-    if(date.getFullYear() > new Date(user.dob as Date).getFullYear()){
-      let age = date.getFullYear() - new Date(user.dob as Date).getFullYear() -  1;
-      if(date.getMonth() > new Date(user.dob as Date).getMonth()){
+
+    if (date.getFullYear() > new Date(user.dob as Date).getFullYear()) {
+      let age = date.getFullYear() - new Date(user.dob as Date).getFullYear() - 1;
+      if (date.getMonth() > new Date(user.dob as Date).getMonth()) {
         age++;
       }
-      else if(date.getMonth() === (new Date(user.dob as Date).getMonth())){
-        if(date.getDate() >= new Date(user.dob as Date).getDate()){
+      else if (date.getMonth() === (new Date(user.dob as Date).getMonth())) {
+        if (date.getDate() >= new Date(user.dob as Date).getDate()) {
           age++;
         }
       }
       user.age = age;
       console.log(user);
-      
+
     }
     user.name = user.firstName + " " + user.lastName;
     let chronicIllnesses = [];
-    for(let i=0; i<user.patientChronicIllness.length; i++){
+    for (let i = 0; i < user.patientChronicIllness.length; i++) {
       let chronicIllness = {
-        "chronicIllness" : {
-          "id" : parseInt(user.patientChronicIllness[i].name)
+        "chronicIllness": {
+          "id": parseInt(user.patientChronicIllness[i].name)
         },
-        "yearsOfIllness" : user.patientChronicIllness[i].yearsOfIllness
+        "yearsOfIllness": user.patientChronicIllness[i].yearsOfIllness
       }
       chronicIllnesses.push(chronicIllness);
     }
     user.patientChronicIllness = chronicIllnesses;
 
     console.log("sanket");
-    if(this.tokenRole === "PATIENT"){
+    if (this.tokenRole === "PATIENT") {
       console.log("mihir");
       user.role = this.user?.role;
+      user.email = this.user?.email;
       console.log(user);
-      
-      this.userService.updateUser(user, this.id).subscribe((data)=> {
-        console.log(data);
+
+      this.userService.updateUser(user, this.id).subscribe((data) => {
+        console.log("updateUser DATA", data);
         this.toggleDisable();
+        // this.user = data;
+        // location.reload();
+        this.cdr.detectChanges();
+        // this.user.role = 
+        
+        
+        this.userService.getUser().subscribe(data => {
+          this.user = data;
+          // this.editForm.valueChanges.subscribe(data => {
+          //   this.user = data;
+          //   console.log(this.user);
+          // });
+        });
+
       });
     }
-    else if(this.tokenRole === "DOCTOR"){
+    else if (this.tokenRole === "DOCTOR") {
       let userId = 0;
       // this.userService.registerUser(signupDetail).subscribe((data) => {
       //   this.authToken = data;
@@ -190,53 +205,50 @@ export class ProfilePageComponent implements OnInit, OnDestroy {
       //   signupDetail = {email : "", password : ""};
       // });
       user.role = this.doctor?.user.role;
-      let doctor : Doctor = {
-        "user" : user,
-        "qualification" : this.editForm.value.qualification,
-        "casesSolved" : this.editForm.value.casesSolved,
-        "available" : true,
-        "department" : {
-          id : 1
+      user.email = this.doctor?.user.email;
+      let doctor: Doctor = {
+        "user": user,
+        "qualification": this.editForm.value.qualification,
+        "casesSolved": this.editForm.value.casesSolved,
+        "available": true,
+        "department": {
+          id: 1
         }
       };
       console.log("=======================================");
       
       console.log(doctor);
-      this.userService.updateDoctor(doctor, this.id).subscribe((data)=> {
+      this.userService.updateDoctor(doctor, this.id).subscribe((data) => {
         // const authToken = data;
         // console.log(authToken.token);
         console.log(data);
+        this.doctorService.getDoctor(this.id).subscribe(data => {
+          this.doctor = data;
+          this.user = data.user;
+          this.cdr.detectChanges();
+        });
         this.toggleDisable();
-        
       });
     }
-    
-    this.userService.getUser().subscribe(data => {
-      this.user = data;
-      this.editForm.valueChanges.subscribe(data => {
-        this.user = data;
-      });
-    });
 
-    this.doctorService.getDoctor(this.id).subscribe(data => {
-      this.doctor = data;
-    });
-    
-    
+
+
+
+
   }
 
   toggleDisable() {
     this.disable = !this.disable;
     // update the disable property of each form field
     Object.keys(this.editForm.controls).forEach(key => {
-        if (this.disable) {
-            this.editForm.controls[key].disable();
-        } else {
-          if(key !== "casesSolved"){
-            this.editForm.controls[key].enable();
-          }
-          
+      if (this.disable) {
+        this.editForm.controls[key].disable();
+      } else {
+        if (key !== "casesSolved" && key !== "email") {
+          this.editForm.controls[key].enable();
         }
+
+      }
     });
   }
 }
