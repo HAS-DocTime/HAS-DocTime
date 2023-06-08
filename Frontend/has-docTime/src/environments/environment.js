35dbff50db3bcfade2cebc5345c3b6bf875d6b97
arpit <abhavsar@argusoft.com>
2023-06-08 09:56:49 +0530

    [Test]

--- Frontend/has-docTime/src/environments/environment.js
+++ Frontend/has-docTime/src/environments/environment.js
@@ -2,6 +2,6 @@
 exports.__esModule = true;
 exports.environment = void 0;
 exports.environment = {
-    apiUrl: "http://192.1.150.86:8080/",
-    host: "192.1.150.86"
+    apiUrl: "http://192.1.200.177:8080/",
+    host: "192.1.200.177"
 };
--- Frontend/has-docTime/src/environments/environment.ts
+++ Frontend/has-docTime/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  apiUrl : "http://192.1.150.86:8080/",
-  host : "192.1.150.86",
+  apiUrl : "http://192.1.200.177:8080/",
+  host : "192.1.200.177",
   firebase: {
     apiKey: "AIzaSyA0MKVpCrRX1r31P_9jlkYdWJbZ1oeW4Zo",
     authDomain: "ng-hasdoctime-images.firebaseapp.com",
@@ -10,4 +10,4 @@ export const environment = {
     appId: "1:974706766466:web:7aaaf2bf0ab2fc61ebba9b",
     measurementId: "G-2M1LTWJB3T"
   }
-} 
+}
