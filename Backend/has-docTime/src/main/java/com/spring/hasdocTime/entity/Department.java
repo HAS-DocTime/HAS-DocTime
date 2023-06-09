1ed06307671eb3ed511ee9cdb8966bf4d80d6979
Dhruv <dsindha@argusoft.com>
2023-06-09 11:40:14 +0530

    revert

--- Backend/has-docTime/src/main/java/com/spring/hasdocTime/entity/Department.java
+++ Backend/has-docTime/src/main/java/com/spring/hasdocTime/entity/Department.java
@@ -6,7 +6,10 @@ package com.spring.hasdocTime.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
