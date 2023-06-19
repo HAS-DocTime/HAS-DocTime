c48a3511fbe8b63ce298ecfa22cfaf9353d0d5fe
SanketRathod666 <srathod@argusoft.com>
2023-06-19 15:29:18 +0530

    [added lazy load in the profile component's profile picture]

--- Frontend/has-docTime/src/app/components/profile-dashboard/profile-page/profile-page.component.ts
+++ Frontend/has-docTime/src/app/components/profile-dashboard/profile-page/profile-page.component.ts
@@ -59,6 +59,9 @@ export class ProfilePageComponent implements OnInit {
   imageUrl: string = "";
   isLoading: boolean = false;
   currentUrl! : any;
+  isImageLoaded : boolean = false;
+  placeHolderImage : string = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+
 
   ngOnInit(): void {
 
